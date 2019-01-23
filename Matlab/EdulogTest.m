4b0b4d21cc9db23fce4bc0002091541e1eeaf21f
Todd Parsons <todd.e.parsons@googlemail.com>
2019-01-23 15:12:24 +0000

    Removed some obsolete code

--- Matlab/EdulogTest.m
+++ Matlab/EdulogTest.m
@@ -27,29 +27,23 @@ function [test, tFig] = EdulogTest(port, dur, sps, loggers)
 % the graph can be changed by editing this object.
 
 input('Press ''Enter'' to test eduloggers...\n', 's');
+cont = false;
 
-if isempty(start)
-    testcycle = 'No';
-    cont = false;
-    while cont == false
-        switch testcycle
-            case 'No'
-                [test, start] = EdulogRun(port, dur, sps, loggers);
-                tFig = EdulogPlot(test, loggers);
-                testcycle = questdlg('Does this data look reasonable?','Test Eduloggers','Yes','No', 'Cancel','Yes');
-                close all
-            case 'Yes'
-                cont = true;
-                close all
-            case 'Cancel'
-                cont = true;
-                close all
-                error('Edulogger test cancelled')
-        end
+while cont == false
+    switch testcycle
+        case 'No'
+            test = EdulogRun(port, dur, sps, loggers);
+            tFig = EdulogPlot(test, loggers);
+            testcycle = questdlg('Does this data look reasonable?','Test Eduloggers','Yes','No', 'Cancel','Yes');
+            close all
+        case 'Yes'
+            cont = true;
+            close all
+        case 'Cancel'
+            cont = true;
+            close all
+            error('Edulogger test cancelled')
     end
-else
-    error('Edulogger test cancelled')
-    test = NaN;
-    tFig = NaN;
 end
+
 end
