4f99dea283eb39a38e12c74ff6a43864e9385cca
Todd Parsons <todd.e.parsons@googlemail.com>
2019-10-09 15:42:26 +0100

    Split tonic and phasic

--- Matlab/elmarkevents.m
+++
@@ -1,9 +0,0 @@
-function data = elmarkevents(data, events)
-
-if islogical(events) % If supplied events parameter is a logical matrix...
-    events = find(events); % Convert to nunmeric indices
-end
-for ev = events % For each event...
-    [~, i] = min(abs([data.Time] - ev)); % Find the timestamp closest to the event
-    data(i).Event = true; % Mark an event at that time
-end
