27ca5c13d58b2a8cf00438f55ff480626fcd14c0
Todd Parsons <todd.e.parsons@googlemail.com>
2019-10-17 12:13:29 +0100

    Edit to cope with non responders

--- Matlab/elxcorr.m
+++ Matlab/elxcorr.m
@@ -19,7 +19,7 @@ function [r, lag] = elxcorr(data, correlates)
 for c = correlates % For each corrlate...
     ts.(c{:}) = [data([data.(c{:})]).Time]; % Extract timestamps of events
     if isempty(ts.(c{:}))
-        rmfield(ts, c{:})
+        rmfield(ts, c{:});
         correlates(strcmp(correlates, c{:})) = [];
     end
 end
