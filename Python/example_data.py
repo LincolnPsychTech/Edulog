f23ba2d88dfa6aaf28064d89f344118c22683db5
Todd Parsons <todd.e.parsons@googlemail.com>
2019-12-06 13:21:37 +0000

    Added example data generator (temp)

---
+++ Python/example_data.py
@@ -0,0 +1,12 @@
+import math
+import random
+import pandas
+import edulog as el
+
+data = list()
+for n in range(100):
+    data.append({"GSR": abs(math.sin(n)*(1-random.random()**2)), "Time": n/5})
+data = pandas.DataFrame(data)
+
+data = el.gsrsplit(data)
+data = el.scr(data, 'median')
