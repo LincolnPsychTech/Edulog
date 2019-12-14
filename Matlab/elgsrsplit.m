4e78e52621c1d939bffd1684ca2638eb4c0d5dd6
Todd Parsons <todd.e.parsons@googlemail.com>
2019-12-14 13:27:37 +0000

    Expanded smoothing window to fit literature

--- Matlab/elgsrsplit.m
+++ Matlab/elgsrsplit.m
@@ -17,7 +17,7 @@ function data = elgsrsplit(data)
 
 if isfield(data, 'GSR') % Throw an error if data supplied is not GSR
     sps = round( 1/mean( diff([data.Time]) ) ); % From data, estimate sps which was used
-    t = smooth([data.GSR], sps*2, 'moving'); % Use a moving smooth method with a span of 2 seconds to extract tonic signal
+    t = smooth([data.GSR], sps*8, 'moving'); % Use a moving smooth method with a span of 2 seconds to extract tonic signal
     for n = 1:length(data) % For each data point...
         data(n).Tonic = t(n); % Replace data in tonic array with smoothed equivalent
         data(n).Phasic = data(n).GSR - t(n); % Replace data in phasic array with difference between tonic equivalent and original value (this is the phasic signal)
